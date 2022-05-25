31760adc4939ed574ef456160a1173f84486fb53
arnoosha <arnooshanajafi26@gmail.com>
2022-05-26 01:28:36 +0430

    upgrading login and signUp button

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,18 @@ class MyApp extends StatelessWidget {
           child: Column(children: [
             Container(margin: EdgeInsets.only(top: 450),
             child: FlatButton(
+              padding: EdgeInsets.symmetric(vertical: 18.0 , horizontal: 60.0),
               onPressed: () => Scaffold(),
               child: Text('Login'),
             shape: RoundedRectangleBorder(side: BorderSide(color: Colors.blueAccent)),
-            ),)
+            ),),
+            Container(margin: EdgeInsets.only(top: 20),
+              child: FlatButton(
+                padding: EdgeInsets.symmetric(vertical: 18.0 , horizontal: 60.0),
+                onPressed: () => Scaffold(),
+                child: Text('Sign Up'),
+                shape: RoundedRectangleBorder(side: BorderSide(color: Colors.blueAccent)),
+              ),),
           ],),
         ),
         appBar: AppBar(
