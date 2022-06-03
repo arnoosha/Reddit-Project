4083a8f69a87f5e0c494b5b88719c33e9c03985f
KMBabak <babak.fathi.s81@gmail.com>
2022-06-03 21:50:30 +0430

    Datetime added to posts

--- lib/classes/Post.dart
+++ lib/classes/Post.dart
@@ -3,7 +3,7 @@ class Post {
   String postTitle ;
   String postDescription ;
   int postLikes = 0 ;
-  Post({this.postTitle, this.postDescription , this.postLikes});
+  Post({this.postTitle, this.postDescription , this.postLikes , this.creationDate});
   bool isPositiveDetect(){
     if (postLikes < 0) {return true;}
     else {return  false;}
@@ -11,10 +11,25 @@ class Post {
   bool isliked = false;
   bool isdisliked = false;
   bool isSaved = false ;
+  DateTime creationDate ;
 }
 
 List<Post> postsList = [
-  Post(postTitle: "My last Valorant Match" , postDescription: "pashmam" , postLikes: 0),
-  Post(postTitle: "My 2 last Valorant Match" , postDescription: "I won 13-4" , postLikes: 0),
-  Post(postTitle: "My 3 last Valorant Match" , postDescription: "I won 13-0" , postLikes: 0),
+  Post(postTitle: "My last Valorant Match" , postDescription: "pashmam" , postLikes: 0 , creationDate: DateTime(2022, 5 , 20)),
+  Post(postTitle: "My 2 last Valorant Match" , postDescription: "I won 13-4" , postLikes: 0 ,creationDate: DateTime(2022, 5 , 28) ),
+  Post(postTitle: "My 3 last Valorant Match" , postDescription: "I won 13-0" , postLikes: 0 , creationDate: DateTime(2022, 5 , 26)),
 ];
+List<Post> bubbleSort(List<Post> postsList) {
+  List<Post> result = postsList;
+  for (int i = 0; i < result.length; i++) {
+    for (int j = 0; j < result.length - i - 1; j++) {
+      if (result[j].creationDate.isBefore(result[j + 1].creationDate)) {
+        Post temp = result[j];
+        result[j] = result[j + 1];
+        result[j + 1] = temp;
+      }
+    }
+  }
+  postsList = result;
+  return postsList;
+}
