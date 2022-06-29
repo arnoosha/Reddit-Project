c3ffd660b7547276b659d943ea9ae49677d5213f
arnoosha <arnooshanajafi26@gmail.com>
2022-06-29 16:57:42 +0430

    saving changes

--- lib/classes/User.dart
+++ lib/classes/User.dart
@@ -4,17 +4,17 @@ import 'Forum.dart';
 
 class User {
 
-  List<Post> savedPosts = [];
-  List<Forum> followedForums = [];
+  List<Post> savedPosts = [] ;
+  List<Forum> followedForums = [] ;
   String username ;
   String password ;
-  String bio;
+  String bio ;
   String userPicture ;
   int followers = 0;
   int followings = 0 ;
 
-  User(this.username, this.password , this.userPicture);
+  User(this.username , this.password , this.userPicture) ;
 }
 
-User Babak = User("Babak", "2663" ,"assets/images/6.png");
-User Ari = User("Arnoosha", "123456" , "assets/images/5.png");
+User Babak = User("Babak", "2663" ,"assets/images/6.png") ;
+User Ari = User("Arnoosha", "123456" , "assets/images/5.png") ;
